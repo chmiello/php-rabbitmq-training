2fcd361ecfd7575af0a2a9e9d993e0b3471b4b46
chmiello <bartek@chmiello.pl>
2019-05-19 22:50:13 +0200

    #3 Creating config helper

---
+++ bootstrap/helpers.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chmiello
+ * Date: 19.05.19
+ * Time: 22:07
+ */
+
+if (!function_exists('config')) {
+    /**
+     * @param $name
+     */
+    function config($name)
+    {
+        $nameArray = explode('.', $name);
+
+        // first element - file name
+
+        if (file_exists(HOME . 'config/' . $nameArray[0] . PHP_EXT)) {
+
+            $fileConfig = require_once HOME . 'config/' . $nameArray[0] . PHP_EXT;
+
+            $nameArray = array_slice($nameArray, 1);
+            $configItem = false;
+            foreach ($nameArray as $item) {
+                if (isset($fileConfig[$item])) {
+                    $configItem = $fileConfig[$item];
+                } else {
+                    $configItem = false;
+                }
+            }
+
+            return $configItem;
+        }
+
+        return false;
+    }
+}
---
+++ config/app.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chmiello
+ * Date: 19.05.19
+ * Time: 21:08
+ */
+
+return [
+    'commands' => [
+        
+    ],
+];
